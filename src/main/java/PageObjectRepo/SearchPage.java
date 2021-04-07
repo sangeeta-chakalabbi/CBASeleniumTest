581da2bed48178b00b9a72ce513c7e4a52f9f9e0
sangeeta-chakalabbi <48641838+sangeeta-chakalabbi@users.noreply.github.com>
2021-04-07 15:14:17 +1000

    first test

--- src/main/java/PageObjectRepo/SearchPage.java
+++ src/main/java/PageObjectRepo/SearchPage.java
@@ -1,5 +1,40 @@
 package PageObjectRepo;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+
+
+
 public class SearchPage {
+	
+
+
+	org.openqa.selenium.support.ui.WebDriverWait wait = new org.openqa.selenium.support.ui.WebDriverWait(SetUp.driver,30);
+
+
+	@FindBy(id="search-input")
+	private WebElement searchInputField;
+	
+	
+	
+	
+	public SearchPage(WebDriver driver) {
+		PageFactory.initElements(driver, this);
+	}
+
+
+	public WebElement getSearchInputField() {
+    	wait.until(ExpectedConditions.elementToBeClickable(searchInputField));
+		return searchInputField;
+	}
+
+
+	public void setSearchInputField(WebElement searchInputField) {
+		this.searchInputField = searchInputField;
+	}
 
 }
--- src/test/java/tests/SearchTest.java
+++ src/test/java/tests/SearchTest.java
@@ -1,12 +1,16 @@
 package tests;
 
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import PageObjectRepo.*;
+import init.SetUp;
 
 public class SearchTest extends SetUp {
-	
+	SearchPage searchPage = new SearchPage(SetUp.driver);
+
+
 	
 	@BeforeMethod
 	public void start() {
@@ -19,9 +23,11 @@ public class SearchTest extends SetUp {
 	
 	
 	@Test
-	public void test1() {
-		
+	public void test_queryInputFieldIsPresent() {
+		Assert.assertTrue(searchPage.getSearchInputField().isDisplayed());
 	}
+	
+	
 	
 	@AfterMethod
 	public void end() {
